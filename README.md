6a485b5385b1909c38f580bec9b0d6aadc657f20
Gerard Bentley <gerardrbentley@gmail.com>
2022-04-25 12:02:01 -0700

    feat: duck load

---
+++ README.md
@@ -0,0 +1,53 @@
+[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/gerardrbentley/uber-nyc-pickups-duckdb/main)
+
+# Streamlit + DuckDB Demo: Uber Pickups in New York City
+
+Inspired / Copied from [streamlit demo repo](https://github.com/streamlit/demo-uber-nyc-pickups)
+
+A [Streamlit](https://streamlit.io) demo converted to utilize [DuckDB](https://duckdb.org/docs/api/python) to run data analysis faster and on more data than raw pandas.
+
+Check out the speed up on loading data.
+From left to right:
+
+- `5.087 s`: [streamlit example (100,000 rows)](https://github.com/streamlit/demo-uber-nyc-pickups/blob/e714e117abe0a22fe159ce7b29980c566289b6d1/streamlit_app.py#L32)
+- `54.306 s`: streamlit example (Full Dataset using `pd.read_csv`)
+- `1.178 s`: this example (Full Dataset using `pyarrow` + `duckdb`)
+
+![load data speedup compare](load_data_compare.png)
+
+(*Note:* Profiled with [pyinstrument](https://pyinstrument.readthedocs.io/en/latest/how-it-works.html), see more on the caveats / how it works in [this post](http://joerick.me/posts/2017/12/15/pyinstrument-20/))
+
+## Analysis
+
+*TODO:* Compare filtering and averaging midpoint with duckdb
+
+I wrote the `load_data` function above to match what the original code does, which is **load** the data into a `Dataframe`, not just load the schema.
+After it's loaded then `pandas` and `numpy` are used for some additional filtering and computation.
+
+The real point of `duckdb` is to do your filtering and computation **before** loading all of your data into memory.
+
+For parity with the streamlit demo `load_data` looks like:
+
+```py
+    data = duckdb.arrow(data)
+    return data.arrow().to_pandas()
+```
+
+Just returning the duckdb instance will drop the time `load_data` takes to `~0.1 s`!
+Then you have an in memory analysis object ready to go.
+
+```py
+    data = duckdb.arrow(data)
+    return data
+```
+
+## Run this demo locally
+
+```sh
+pip install -r https://raw.githubusercontent.com/gerardrbentley/uber-nyc-pickups-duckdb/main/requirements.txt
+streamlit run https://raw.githubusercontent.com/gerardrbentley/uber-nyc-pickups-duckdb/main/streamlit_app.py
+```
+
+### Questions? Comments?
+
+Please ask in the [Streamlit community](https://discuss.streamlit.io).
---
+++ streamlit_app_duck.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2022 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""An example of showing geographic data."""
+
+from datetime import datetime
+from pathlib import Path
+import streamlit as st
+import pandas as pd
+import duckdb
+from pyarrow import csv
+import numpy as np
+import altair as alt
+import pydeck as pdk
+from pyinstrument import Profiler
+
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout="wide")
+
+profiler = Profiler(interval=0.0001)
+profiler.start()
+
+# LOAD DUCKDB ONCE
+@st.experimental_singleton
+def load_data():
+    data = csv.read_csv('uber-raw-data-sep14.csv.gz', convert_options=csv.ConvertOptions(
+        include_columns=["Date/Time","Lat","Lon"],
+        timestamp_parsers=['%m/%d/%Y %H:%M:%S']
+    )).rename_columns(['date/time', 'lat', 'lon'])
+
+    # We transform the dataset into a DuckDB relation
+    data = duckdb.arrow(data)
+    return data.arrow().to_pandas()
+
+
+# FUNCTION FOR AIRPORT MAPS
+def map(data, lat, lon, zoom):
+    st.write(
+        pdk.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={
+                "latitude": lat,
+                "longitude": lon,
+                "zoom": zoom,
+                "pitch": 50,
+            },
+            layers=[
+                pdk.Layer(
+                    "HexagonLayer",
+                    data=data,
+                    get_position=["lon", "lat"],
+                    radius=100,
+                    elevation_scale=4,
+                    elevation_range=[0, 1000],
+                    pickable=True,
+                    extruded=True,
+                ),
+            ],
+        )
+    )
+
+
+# FILTER DATA FOR A SPECIFIC HOUR, CACHE
+@st.experimental_memo
+def filterdata(df, hour_selected):
+    return df[df["date/time"].dt.hour == hour_selected]
+
+
+# CALCULATE MIDPOINT FOR GIVEN SET OF DATA
+@st.experimental_memo
+def mpoint(lat, lon):
+    return (np.average(lat), np.average(lon))
+
+
+# FILTER DATA BY HOUR
+@st.experimental_memo
+def histdata(df, hr):
+    filtered = data[
+        (df["date/time"].dt.hour >= hr) & (df["date/time"].dt.hour < (hr + 1))
+    ]
+
+    hist = np.histogram(filtered["date/time"].dt.minute, bins=60, range=(0, 60))[0]
+
+    return pd.DataFrame({"minute": range(60), "pickups": hist})
+
+
+# STREAMLIT APP LAYOUT
+data = load_data()
+# LAYING OUT THE TOP SECTION OF THE APP
+row1_1, row1_2 = st.columns((2, 3))
+
+with row1_1:
+    st.title("NYC Uber Ridesharing Data")
+    hour_selected = st.slider("Select hour of pickup", 0, 23)
+
+with row1_2:
+    st.write(
+        """
+    ##
+    Examining how Uber pickups vary over time in New York City's and at its major regional airports.
+    By sliding the slider on the left you can view different slices of time and explore different transportation trends.
+    """
+    )
+
+# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
+row2_1, row2_2, row2_3, row2_4 = st.columns((2, 1, 1, 1))
+
+# SETTING THE ZOOM LOCATIONS FOR THE AIRPORTS
+la_guardia = [40.7900, -73.8700]
+jfk = [40.6650, -73.7821]
+newark = [40.7090, -74.1805]
+zoom_level = 12
+midpoint = mpoint(data["lat"], data["lon"])
+
+with row2_1:
+    st.write(
+        f"""**All New York City from {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+    )
+    map(filterdata(data, hour_selected), midpoint[0], midpoint[1], 11)
+
+with row2_2:
+    st.write("**La Guardia Airport**")
+    map(filterdata(data, hour_selected), la_guardia[0], la_guardia[1], zoom_level)
+
+with row2_3:
+    st.write("**JFK Airport**")
+    map(filterdata(data, hour_selected), jfk[0], jfk[1], zoom_level)
+
+with row2_4:
+    st.write("**Newark Airport**")
+    map(filterdata(data, hour_selected), newark[0], newark[1], zoom_level)
+
+# CALCULATING DATA FOR THE HISTOGRAM
+chart_data = histdata(data, hour_selected)
+
+# LAYING OUT THE HISTOGRAM SECTION
+st.write(
+    f"""**Breakdown of rides per minute between {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+)
+
+st.altair_chart(
+    alt.Chart(chart_data)
+    .mark_area(
+        interpolate="step-after",
+    )
+    .encode(
+        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("pickups:Q"),
+        tooltip=["minute", "pickups"],
+    )
+    .configure_mark(opacity=0.2, color="red"),
+    use_container_width=True,
+)
+profiler.stop()
+
+profiler.print()
+(Path('profiles') / f"Duck_Load_{datetime.now().isoformat()}.html").write_text(profiler.output_html())
