c40f222a0e1bdeb2cfde8671a1ee26e4e31f1142
Gerard Bentley <gerardrbentley@gmail.com>
2022-04-28 19:57:30 -0700

    feat: download ursa labs taxi data

---
+++ 00_download_nyc_data.py
@@ -0,0 +1,28 @@
+import time
+import boto3
+import os
+from botocore import UNSIGNED
+from botocore.client import Config
+from botocore.exceptions import ResponseStreamingError
+s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
+
+def download_s3_folder(bucket_name, s3_folder, local_dir=None):
+    bucket = s3.Bucket(bucket_name)
+    for obj in bucket.objects.filter(Prefix=s3_folder):
+        target = obj.key if local_dir is None \
+            else os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
+        if os.path.exists(target):
+            print(f"skipping {target}")
+            continue
+        if not os.path.exists(os.path.dirname(target)):
+            os.makedirs(os.path.dirname(target))
+        if obj.key[-1] == '/':
+            continue
+        print(f'downloading {target}')
+        try:
+            bucket.download_file(obj.key, target)
+        except ResponseStreamingError:
+            time.sleep(60)
+            bucket.download_file(obj.key, target)
+
+download_s3_folder('ursa-labs-taxi-data', '', 'nyc-taxi')
