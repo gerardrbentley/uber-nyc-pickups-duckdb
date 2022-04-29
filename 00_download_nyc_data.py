9b3693c6e0dc6501b3a128273167e366dc63282c
Gerard Bentley <gerardrbentley@gmail.com>
2022-04-29 08:45:38 -0700

    log: begin statement

--- 00_download_nyc_data.py
+++ 00_download_nyc_data.py
@@ -31,4 +31,5 @@ def download_s3_folder(bucket_name, s3_folder, local_dir=None):
             bucket.download_file(obj.key, target)
 
 if __name__ == '__main__':
+    print("BEGIN DOWNLOAD")
     download_s3_folder("ursa-labs-taxi-data", "", "nyc-taxi")
